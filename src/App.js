d835335d6cb3d6865b506e46aa160be2cf2f9004
zellko <wyttenda@tutanota.com>
2022-08-17 12:36:56 +0200

    Added Nav to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
+import Nav from './components/Nav';
 import './styles/App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
