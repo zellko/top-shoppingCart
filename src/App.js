c068c610860fcb929589d00089222983d6818f87
zellko <wyttenda@tutanota.com>
2022-08-17 12:23:27 +0200

    Created Draft Home&Shop&Cart and added rooting
    Created DRAFT Home & Shop & Cart components
    
    Added rooting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Cart from './components/Cart';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      App.js
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
