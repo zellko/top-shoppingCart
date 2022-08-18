d19364aa16fb2a9fd378c54f830622a83319c8f3
zellko <wyttenda@tutanota.com>
2022-08-18 17:07:25 +0200

    Modified App.js
    Added CartHook hook
    Added props to Nav, Shop, Cart routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
 import Nav from './components/Nav';
+import CartHook from './components/CartHook';
 import './styles/App.css';
 
 function App() {
+  const {
+    cart,
+    addItemToCart,
+    removeItemFromCart,
+    updateItemQty,
+  } = CartHook();
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav />
+        <Nav cartLength={cart.length} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop" element={<Shop onAddToCart={addItemToCart} />} />
+          <Route
+            path="/cart"
+            element={(
+              <Cart
+                cart={cart}
+                onRemoveItem={removeItemFromCart}
+                onUpdateQuantity={updateItemQty}
+              />
+            )}
+          />
         </Routes>
       </div>
     </BrowserRouter>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('App component test', () => {
-  it.todo('test A');
+  it('render Home page', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('render Shop page', async () => {
+    const { container } = render(<App />);
+
+    const homeButton = screen.getByRole('button', { name: 'Go Shopping' });
+    expect(homeButton).toBeInTheDocument();
+
+    userEvent.click(homeButton);
+
+    await waitFor(() => screen.getByRole('heading', { name: 'PRODUCT' }));
+
+    const shopHeading = screen.getByRole('heading', { name: 'PRODUCT' });
+
+    expect(shopHeading).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('render Cart page', async () => {
+    const { container } = render(<App />);
+
+    const cartButton = screen.getAllByRole('link')[2];
+    expect(cartButton).toBeInTheDocument();
+
+    userEvent.click(cartButton);
+
+    await waitFor(() => screen.getByRole('heading', { name: 'Your shopping cart is empty' }));
+
+    const cartHeading = screen.getByRole('heading', { name: 'Your shopping cart is empty' });
+
+    expect(cartHeading).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
 });
